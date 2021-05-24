ea622bc93ad92704ffd6453d8a3280710f781cf3
unknown <lamnguyen02122004@gmail.com>
2021-05-24 17:19:17 +0700

    Add vidu2

---
+++ src/hw1/Vidu2.java
@@ -0,0 +1,10 @@
+package hw1;
+
+public class Vidu2 {
+    public static void main(String[] args) {
+        int x=2;
+        int y=5;
+        int sum = x+y ;
+        System.out.println("Tong cua x va y la : " + sum);
+    }
+}
